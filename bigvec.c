25ee910186db4fe858d555f60b26cbadf539ac90
Ankit Srivastava <asrivast@gatech.edu>
2014-10-01 23:35:17 -0400

    Implemented all-gather part of scatter-gather algorithm.

--- bigvec.c
+++ bigvec.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <mpi.h>
+#include <stdlib.h>
 
 static void scatter (int* data, const int len);
 static void allgather (int* data, const int len);
@@ -29,20 +30,19 @@ static void scatter (int* data, const int len)
   for (int offset = P/2; offset >= 1; offset >>= 1) {
     if ((rank % offset) == 0) {
       if (((rank / offset) % 2) == 0) {
-	int* msg_send = data + (rank + offset)*len_per_proc;
-	const int msg_len = len_per_proc * offset;
-	const int rank_dest = rank + offset;
-	const int msg_tag = 1000 + rank_dest; /* must match receiver's! */
-	MPI_Send (msg_send, msg_len, MPI_INT, rank_dest, msg_tag,
-		  MPI_COMM_WORLD);
-      } else {
-	MPI_Status stat;
-	int* msg_recv = data + rank*len_per_proc;
-	const int msg_len = len_per_proc * offset;
-	const int rank_source = rank - offset;
-	const int msg_tag = 1000 + rank; /* must match sender's! */
-	MPI_Recv (msg_recv, msg_len, MPI_INT, rank_source, msg_tag,
-		  MPI_COMM_WORLD, &stat);
+        int* msg_send = data + (rank + offset)*len_per_proc;
+        const int msg_len = len_per_proc * offset;
+        const int rank_dest = rank + offset;
+        const int msg_tag = 1000 + rank_dest; /* must match receiver's! */
+        MPI_Send (msg_send, msg_len, MPI_INT, rank_dest, msg_tag, MPI_COMM_WORLD);
+      } 
+      else {
+        MPI_Status stat;
+        int* msg_recv = data + rank*len_per_proc;
+        const int msg_len = len_per_proc * offset;
+        const int rank_source = rank - offset;
+        const int msg_tag = 1000 + rank; /* must match sender's! */
+        MPI_Recv (msg_recv, msg_len, MPI_INT, rank_source, msg_tag, MPI_COMM_WORLD, &stat);
       }
     }
   }
@@ -60,7 +60,21 @@ static void allgather (int* data, const int len)
   const int rank_next = (rank + 1) % P;
   const int rank_prev = (rank + P - 1) % P;
 
-  /* ===== Your implementation goes here ===== */
+  MPI_Request* requests = (MPI_Request*) malloc (P * sizeof (MPI_Request));
+
+  for (int offset = P; offset > 0; --offset) {
+    int* msg_send = data + ((rank + offset) % P) * len_per_proc; 
+    int* msg_recv = data + ((rank + offset + P - 1) % P) * len_per_proc;
+    const int send_tag = 1000 + rank + offset;
+    const int recv_tag = 1000 + rank_prev + offset;
+    MPI_Status stat;
+    MPI_Isend (msg_send, len_per_proc, MPI_INT, rank_next, send_tag, MPI_COMM_WORLD, &requests[offset-1]);
+    MPI_Recv (msg_recv, len_per_proc, MPI_INT, rank_prev, recv_tag, MPI_COMM_WORLD, &stat);
+  }
+  MPI_Status *statuses = (MPI_Status*) malloc (P * sizeof(MPI_Status));
+  MPI_Waitall (P, requests, statuses);
+  free (requests);
+  free (statuses);
 }
 
 const char* bcast_algorithm (void)
